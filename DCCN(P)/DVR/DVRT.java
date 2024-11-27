4c7fed51501fe4f19cfa5bb5e8bc5a02cef7fe2e
Thirumalesh25 <229x1a0540@gprec.ac.in>
2024-11-27 07:47:03 +0530

    Update DVRT.java

--- DCCN(P)/DVR/DVRT.java
+++ DCCN(P)/DVR/DVRT.java
@@ -32,7 +32,7 @@ class DVRT{
         System.out.println("Shortest Distances : ");
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < n; j++) {
-                System.out.println(dist[i][j]+" ");
+                System.out.print(dist[i][j]+" ");
             }
             System.out.println();
         }
